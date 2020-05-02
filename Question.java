de5a2c3a6a75b54e6ecc4c0ed67eb3c957e17735
neslihansalimoglu <60351697+neslihansalimoglu@users.noreply.github.com>
2020-05-02 05:44:38 -0400

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,24 @@
+public class Question {
+
+    //attributes
+    private String exam;
+    
+    //constructor
+    public Question(String exam){
+        this.exam = exam;
+    }
+
+    //accesors
+    public void setexam(String exam){
+        this.exam = exam;
+    }
+    
+    public String getexam(){
+        return exam;
+    }
+
+    //tostring
+    public String tostring(){
+        return "Exam             : " + exam 
+               
+}
