7e6317ca6b2e7ddd6178ee98a1c4997c2dfe31d0
neslihansalimoglu <60351697+neslihansalimoglu@users.noreply.github.com>
2020-05-02 05:46:51 -0400

    first class

---
+++ BonusQuestion.java
@@ -0,0 +1,13 @@
+public class BonusQuestion {
+
+    public static void main(String[] args) {
+    	System.out.println("I hope I get 100 from this exam. :) ")
+
+        Question exam = new Question("Am I gonna get 100?");
+        
+        exam.getQuestion();
+        
+        System.out.println(exam);
+    }
+    
+}
